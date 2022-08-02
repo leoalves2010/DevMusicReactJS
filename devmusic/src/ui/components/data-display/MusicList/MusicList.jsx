d4186766f702452ffafbb944e9a9e0963995e7f5
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-08-01 21:12:50 -0300

    Criação do componente Timeline

---
+++ devmusic/src/ui/components/data-display/MusicList/MusicList.jsx
@@ -0,0 +1,35 @@
+import styles from "./MusicList.module.css";
+import { TimeService } from "../../../../services/TimeService";
+
+export default function MusicList(props) {
+    function isSelected(music) {
+        return props.selectedMusic === music ? styles["selected"] : "";
+    }
+
+    return (
+        <ul className={styles["music-list"]}>
+            {props.music.map((music) => (
+                <li
+                    key={music.id}
+                    className={[
+                        styles["music-list-item"],
+                        isSelected(music),
+                    ].join(" ")}
+                    onClick={() => props.onSelect(music)}
+                >
+                    <div className={styles["music-information"]}>
+                        <span className={styles["music-name"]}>
+                            {music.name}
+                        </span>
+                        <span className={styles["music-artist"]}>
+                            {music.artist}
+                        </span>
+                        <span className={styles["music-time"]}>
+                            {TimeService.timeDisplay(music.time)}
+                        </span>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+}
--- devmusic/src/ui/pages/index.jsx
+++ devmusic/src/ui/pages/index.jsx
@@ -1,5 +1,35 @@
-export default function Index(){
+import MusicList from "../components/data-display/MusicList/MusicList";
+import Timeline from "../components/data-display/inputs/Timeline/Timeline";
+import { useState } from "react";
+
+const musics = [
+    {
+        id: 1,
+        name: "Música 1",
+        artist: "Artista 1",
+        time: 98,
+    },
+    {
+        id: 2,
+        name: "Música 2",
+        artist: "Artista 2",
+        time: 153,
+    },
+    {
+        id: 3,
+        name: "Música 3",
+        artist: "Artista 3",
+        time: 208,
+    },
+];
+
+export default function Index() {
+    const [width, setWidth] = useState();
+
     return (
-        <div>INDEX</div>
-    )
+        <div>
+            <MusicList music={musics} selectedMusic={musics[0]} onSelect={() => {}}/>
+            <Timeline width={width} onChangeWidth={setWidth}/>
+        </div>
+    );
 }
